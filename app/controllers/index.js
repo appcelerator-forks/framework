aa1cf71ecd33b88047f6cac426da4dc80512e422
lauyikonn <lau.yikonn@gmail.com>
2015-08-04 23:12:06 +0800

    framework!

---
+++ app/controllers/index.js
@@ -0,0 +1,60 @@
+/**
+ * Global Navigation Handler
+ */
+Alloy.Globals.Navigator = {
+	
+	/**
+	 * Handle to the Navigation Controller
+	 */
+	navGroup: $.nav,
+	
+	open: function(controller, payload){
+		var win = Alloy.createController(controller, payload || {}).getView();
+		
+		if(OS_IOS){
+			$.nav.openWindow(win);
+		}
+		else if(OS_MOBILEWEB){
+			$.nav.open(win);
+		}
+		else {
+			
+			// added this property to the payload to know if the window is a child
+			if (payload.displayHomeAsUp){
+				
+				win.addEventListener('open',function(evt){
+					var activity=win.activity;
+					activity.actionBar.displayHomeAsUp=payload.displayHomeAsUp;
+					activity.actionBar.onHomeIconItemSelected=function(){
+						evt.source.close();
+					};
+				});
+			}
+			win.open();
+		}
+	}
+};
+
+/**
+ * Lets add a loading animation - Just for Fun!
+ */
+var loadingView = Alloy.createController("loader");
+loadingView.getView().open();
+loadingView.start();
+
+function loadingViewFinish(){
+	loadingView.finish(function(){
+		if(OS_IOS){
+			$.nav.open();
+		}else{
+			$.index.getView().open();
+		} 
+		init();
+		loadingView = null;
+	});
+}
+
+function init(){
+}
+
+Ti.App.addEventListener('app:loadingViewFinish', loadingViewFinish);
---
+++ app/lib/api.js
@@ -0,0 +1,83 @@
+/*********************
+*** SETTING / API ***
+**********************/
+var API_DOMAIN = "salesad.freejini.com.my";
+
+// APP authenticate user and key
+var USER  = 'salesad';
+var KEY   = '06b53047cf294f7207789ff5293ad2dc';
+
+//API when app loading phase
+var getCategoryList            = "http://"+API_DOMAIN+"/api/getCategoryList?user="+USER+"&key="+KEY;
+var getFeaturedBanner   	   = "http://"+API_DOMAIN+"/api/getFeaturedBannerList?user="+USER+"&key="+KEY;
+var getMerchantList			= "http://"+API_DOMAIN+"/api/getMerchantList?user="+USER+"&key="+KEY;
+var getCategoryAds			= "http://"+API_DOMAIN+"/api/getCategoryAds?user="+USER+"&key="+KEY;
+var getAdsList			= "http://"+API_DOMAIN+"/api/getAdsList?user="+USER+"&key="+KEY;
+var getItemList			= "http://"+API_DOMAIN+"/api/getItemList?user="+USER+"&key="+KEY;
+
+var getMerchantListByCategory  = "http://"+API_DOMAIN+"/api/getMerchantListByCategory?user="+USER+"&key="+KEY;
+//API that call in sequence 
+var APILoadingList = [
+	{url: getCategoryList, model: "category", checkId: "5"},
+	{url: getFeaturedBanner, model: "banners", checkId: "2"},
+	{url: getMerchantList, model: "merchants", checkId: "6"},
+	{url: getCategoryAds, model: "categoryAds", checkId: "7"},
+	{url: getAdsList, model: "ads", checkId: "8"},
+	{url: getItemList, model: "items", checkId: "9"},
+];
+
+/*********************
+**** API FUNCTION*****
+**********************/
+
+exports.loadAPIBySequence = function (ex, counter){ 
+	counter = (typeof counter == "undefined")?0:counter;
+	if(counter >= APILoadingList.length){
+		Ti.App.fireEvent('app:loadingViewFinish');
+		return false;
+	}
+	
+	var api = APILoadingList[counter];
+	var checker = Alloy.createCollection('updateChecker'); 
+	var isUpdate = checker.getCheckerById(api['checkId']);
+	var last_updated ="";
+	
+	//var model = Alloy.createCollection(api['model']);
+	if(isUpdate != "" ){
+		last_updated = isUpdate.updated;
+	}
+	
+	 var url = api['url']+"&last_updated="+last_updated;
+	 console.log(url);
+	 var client = Ti.Network.createHTTPClient({
+	     // function called when the response data is available
+	     onload : function(e) {
+	     	  
+	       var res = JSON.parse(this.responseText);
+	       if(res.status == "Success" || res.status == "success"){
+			/**load new set of category from API**/
+	       	var arr = res.data;
+	       //	console.log(res);
+	        //model.saveArray(arr);
+	       }
+			Ti.App.fireEvent('app:update_loading_text', {text: APILoadingList[counter]['model']+" loading..."});
+			checker.updateModule(APILoadingList[counter]['checkId'],APILoadingList[counter]['model'], Common.now());
+			
+			counter++;
+			API.loadAPIBySequence(ex, counter);
+	     },
+	     // function called when an error occurs, including a timeout
+	     onerror : function(e) {
+	     	console.log("API getCategoryList fail, skip sync with server");
+	     	API.loadAPIBySequence(ex, counter);
+	     },
+	     timeout : 7000  // in milliseconds
+	 });
+	 if(Ti.Platform.osname == "android"){
+	 	client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+	 }
+ 
+	 client.open("POST", url);
+	 // Send the request.
+	client.send();
+};
